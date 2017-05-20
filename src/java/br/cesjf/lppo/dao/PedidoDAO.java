b16c821853ee7540abcaf9e33d6bb2313305b128
Jessica Barbosa <je_barbosaa@hotmail.com>
2017-05-19 22:22:13 -0300

    Adicionar metodos na classe PedidoDAO

--- src/java/br/cesjf/lppo/dao/PedidoDAO.java
+++ src/java/br/cesjf/lppo/dao/PedidoDAO.java
@@ -13,13 +13,19 @@ import java.util.List;
 public class PedidoDAO {
      private final PreparedStatement opNovo;
      private final PreparedStatement opListar;
+     private final PreparedStatement opListarPedido;
+     private final PreparedStatement opTotalPedido;
+     private final PreparedStatement opListarPedidoDono;
      
      
      public PedidoDAO ()throws Exception{
             Connection conexao = ConnectionFactory.createConnection();
                 
-                opNovo  = conexao.prepareStatement("INSERT INTO Pedido (pedido, dono, valor, nome,) VALUES(?, ?, ?, ?)");
-                opListar = conexao.prepareStatement("SELECT * FROM Pedido");
+                opNovo  = conexao.prepareStatement("INSERT INTO pedido (pedido, dono, valor, nome) VALUES(?, ?, ?, ?)");
+                opListar = conexao.prepareStatement("SELECT * FROM pedido");
+                opListarPedido = conexao.prepareStatement("SELECT * FROM pedido WHERE pedido = ? ");
+                opTotalPedido = conexao.prepareStatement("SELECT SUM (valor) AS total FROM pedido WHERE pedido = ?");
+                opListarPedidoDono = conexao.prepareStatement("SELECT * FROM pedido WHERE dono = ? ");
                
      }
      
@@ -28,13 +34,13 @@ public class PedidoDAO {
         opNovo.clearParameters();
         opNovo.setInt(1, novoPedido.getPedido());
         opNovo.setString(2, novoPedido.getDono());
-        opNovo.setFloat(3, novoPedido.getValor());
+        opNovo.setDouble(3, novoPedido.getValor());
         opNovo.setString(4, novoPedido.getNome());
         
         opNovo.executeUpdate();
         
-        }catch (SQLException ex){
-            throw new Exception("Erro ao inserir o contato", ex);
+        }catch (Exception ex){
+            throw new Exception("Erro ao inserir o pedido", ex);
         }
     }
      public List<Pedido> listAll() throws Exception{
@@ -42,14 +48,13 @@ public class PedidoDAO {
             List<Pedido> pedidos = new ArrayList<>();
             
             
-           
             ResultSet resultado = opListar.executeQuery();
                 while(resultado.next()){
                     Pedido novoPedido = new Pedido();
                     novoPedido.setId(resultado.getLong("id"));
                     novoPedido.setPedido(resultado.getInt("pedido"));
                     novoPedido.setDono(resultado.getString("dono"));
-                    novoPedido.setValor(resultado.getFloat("valor"));
+                    novoPedido.setValor(resultado.getDouble("valor"));
                     novoPedido.setNome(resultado.getString("nome"));
                     novoPedido.setAtualizacao(resultado.getTimestamp("atualizacao"));
                     
@@ -59,7 +64,68 @@ public class PedidoDAO {
             
             return pedidos;
         } catch (SQLException ex){
-            throw new Exception("Erro ao listar contatos no banco", ex);
+            throw new Exception("Erro ao listar pedidos no banco", ex);
+        }
+    }
+     public List<Pedido> getByPedido(Long pedido)  throws Exception{
+         
+         try{
+            List<Pedido> pedidos = new ArrayList<>();
+            opListarPedido.setLong(1, pedido);
+            ResultSet resultado = opListarPedido.executeQuery();
+                while(resultado.next()){
+                    Pedido novoPedido = new Pedido();
+                    novoPedido.setId(resultado.getLong("id"));
+                    novoPedido.setPedido(resultado.getInt("pedido"));
+                    novoPedido.setDono(resultado.getString("dono"));
+                    novoPedido.setValor(resultado.getDouble("valor"));
+                    novoPedido.setNome(resultado.getString("nome"));
+                    novoPedido.setAtualizacao(resultado.getTimestamp("atualizacao"));
+                    
+                    pedidos.add(novoPedido);
+                }
+                    
+                 return pedidos;
+        } catch (SQLException ex){
+            throw new Exception("Erro ao buscar contatos no banco", ex);
+            
+        }
+    }
+     public float calculaTotal(Long pedido) throws Exception{
+     try{
+         float valor = 0;
+         opTotalPedido.setLong(1, pedido);
+         ResultSet resultado = opTotalPedido.executeQuery();
+         while(resultado.next()){
+            valor = resultado.getFloat(1);
+         }
+         return valor;
+     }catch (SQLException ex){
+            throw new Exception("Erro ao buscar contatos no banco", ex);
+     }
+    }
+      public List<Pedido> getByDono(String dono)  throws Exception{
+         
+         try{
+            List<Pedido> pedidos = new ArrayList<>();
+            opListarPedido.setString(1, dono);
+            ResultSet resultado = opListarPedido.executeQuery();
+                while(resultado.next()){
+                    Pedido novoPedido = new Pedido();
+                    novoPedido.setId(resultado.getLong("id"));
+                    novoPedido.setPedido(resultado.getInt("pedido"));
+                    novoPedido.setDono(resultado.getString("dono"));
+                    novoPedido.setValor(resultado.getDouble("valor"));
+                    novoPedido.setNome(resultado.getString("nome"));
+                    novoPedido.setAtualizacao(resultado.getTimestamp("atualizacao"));
+                    
+                    pedidos.add(novoPedido);
+                }
+                    
+                 return pedidos;
+        } catch (SQLException ex){
+            throw new Exception("Erro ao buscar contatos no banco", ex);
+            
         }
     }
 }
