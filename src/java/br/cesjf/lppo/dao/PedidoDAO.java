0fac02b82f5110ca8b7da69ee99387711c4a50c5
Jessica Barbosa <je_barbosaa@hotmail.com>
2017-05-22 21:58:49 -0300

    Edição da classe PedidoDAO

--- src/java/br/cesjf/lppo/dao/PedidoDAO.java
+++ src/java/br/cesjf/lppo/dao/PedidoDAO.java
@@ -3,6 +3,7 @@ package br.cesjf.lppo.dao;
 
 import br.cesjf.lppo.Pedido;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,16 +17,23 @@ public class PedidoDAO {
      private final PreparedStatement opListarPedido;
      private final PreparedStatement opTotalPedido;
      private final PreparedStatement opListarPedidoDono;
+     private final PreparedStatement opTotalPedidoDono;
+     private final PreparedStatement opBuscaPorId;
+     private final PreparedStatement opAtualiza;
      
      
      public PedidoDAO ()throws Exception{
             Connection conexao = ConnectionFactory.createConnection();
                 
-                opNovo  = conexao.prepareStatement("INSERT INTO pedido (pedido, dono, valor, nome) VALUES(?, ?, ?, ?)");
-                opListar = conexao.prepareStatement("SELECT * FROM pedido");
-                opListarPedido = conexao.prepareStatement("SELECT * FROM pedido WHERE pedido = ? ");
-                opTotalPedido = conexao.prepareStatement("SELECT SUM (valor) AS total FROM pedido WHERE pedido = ?");
-                opListarPedidoDono = conexao.prepareStatement("SELECT * FROM pedido WHERE dono = ? ");
+                opNovo  = conexao.prepareStatement("INSERT INTO Pedido (pedido, dono, valor, nome) VALUES(?, ?, ?, ?)");
+                opListar = conexao.prepareStatement("SELECT * FROM Pedido");
+                opListarPedido = conexao.prepareStatement("SELECT * FROM Pedido WHERE pedido = ?");
+                opTotalPedido = conexao.prepareStatement("SELECT SUM (valor) AS total FROM Pedido WHERE pedido = ?");
+                opListarPedidoDono = conexao.prepareStatement("SELECT * FROM Pedido WHERE dono = ?");
+                opTotalPedidoDono = conexao.prepareStatement("SELECT SUM (valor) AS total FROM Pedido WHERE dono = ?");
+                opBuscaPorId = conexao.prepareStatement("SELECT * FROM Pedido WHERE id=?");
+                opAtualiza = conexao.prepareStatement("UPDATE Pedido SET pedido = ?, dono = ?, valor = ?, nome = ?, atualizacao = CURRENT_TIMESTAMP WHERE id = ?");
+                
                
      }
      
@@ -108,8 +116,8 @@ public class PedidoDAO {
          
          try{
             List<Pedido> pedidos = new ArrayList<>();
-            opListarPedido.setString(1, dono);
-            ResultSet resultado = opListarPedido.executeQuery();
+            opListarPedidoDono.setString(1, dono);
+            ResultSet resultado = opListarPedidoDono.executeQuery();
                 while(resultado.next()){
                     Pedido novoPedido = new Pedido();
                     novoPedido.setId(resultado.getLong("id"));
@@ -127,5 +135,52 @@ public class PedidoDAO {
             throw new Exception("Erro ao buscar contatos no banco", ex);
             
         }
+      }
+         public float calculaTotalDono(String dono) throws Exception{
+            try{
+                float valor = 0;
+                opTotalPedidoDono.setString(1, dono);
+                ResultSet resultado = opTotalPedidoDono.executeQuery();
+                while(resultado.next()){
+                   valor = resultado.getFloat(1);
+                }
+                return valor;
+            }catch (SQLException ex){
+                   throw new Exception("Erro ao buscar contatos no banco", ex);
+            }
     }
-}
+       public Pedido getById(Long id)  throws Exception{
+        try {
+            Pedido pedido = null;
+            opBuscaPorId.clearParameters();
+            opBuscaPorId.setLong(1, id);
+            ResultSet resultado = opBuscaPorId.executeQuery();
+                if (resultado.next()){
+                    pedido = new Pedido();
+                    pedido.setId(resultado.getLong("id"));
+                    pedido.setPedido(resultado.getInt("pedido"));
+                    pedido.setDono(resultado.getString("dono"));
+                    pedido.setValor(resultado.getDouble("valor"));
+                    pedido.setNome(resultado.getString("nome"));
+                    pedido.setAtualizacao(resultado.getDate("atualizacao"));
+            }
+            return pedido;
+        } catch (SQLException ex){
+            throw new Exception("Erro ao buscar contatos no banco", ex);
+        }
+    }
+        public void atualiza(Pedido pedido) throws Exception {
+        try{
+            opAtualiza.clearParameters();
+            opAtualiza.setInt(1, pedido.getPedido());
+            opAtualiza.setString(2, pedido.getDono());
+            opAtualiza.setDouble(3, pedido.getValor());
+            opAtualiza.setString(4, pedido.getNome());  
+            opAtualiza.setLong(5, pedido.getId());
+            opAtualiza.executeUpdate();
+        } catch (SQLException ex){
+            throw new Exception("Erro ao inserir o contato", ex);
+        }
+       }
+    }
+
